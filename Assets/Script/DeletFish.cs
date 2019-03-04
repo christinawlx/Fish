7ea5c50e58bc4188d5dd9a0260d5948f20835a05
christinawlx <344427042@qq.com>
2019-03-04 00:28:36 -0800

    fish

---
+++ Assets/Script/DeletFish.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DeletFish : MonoBehaviour
+{
+    public int fish_num;
+    public GameObject fish_pref;
+    public List<FishSwim> all_group=new List<FishSwim>();
+    public int[] Die_ID;
+    public Transform background;
+    public Transform[] all_position;
+    public int cur_index=0;
+    public Transform out_screen;
+    Rigidbody testRigid;
+	// Use this for initialization
+	void Start ()
+    {
+        for(int i = 0; i < fish_num; i++)
+        {
+            GameObject temp=Instantiate(fish_pref,transform);
+            temp.GetComponent<FishSwim>().MyInit(i,fish_num-1);
+            all_group.Add(temp.GetComponent<FishSwim>());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            for(int i = 0; i < all_group.Count; i++)
+            {
+                if (!all_group[i].die&&!all_group[i].is_the_last)
+                {
+                    all_group[i].Die();
+                    if (i + 1 < all_group.Count && !all_group[i + 1].is_the_last)
+                    {
+                        all_group[i + 1].Die();
+                    }
+                    break;
+                }
+            }
+            CheckMoveBack();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //GameObject temp = Instantiate(fish_pref, transform);
+            //temp.GetComponent<FishSwim>().MyInit(all_group.Count, fish_num - 1);
+            //temp.transform.position = out_screen.position;
+            for (int i = 0; i < all_group.Count; i++)
+            {
+                if (all_group[i].die)
+                {
+                    all_group[i].Revive();
+                    all_group[i].transform.position = out_screen.position;
+                    break;
+                }
+            }
+        }
+	}
+
+    void CheckMoveBack()
+    {
+        int die_count=0;
+        for(int i = 0; i < all_group.Count; i++)
+        {
+            if (all_group[i].die)
+            {
+                die_count++;
+            }
+        }
+
+        int test_index = 0;
+        for(int i = 0; i < Die_ID.Length; i++)
+        {
+            if (die_count >= Die_ID[i])
+            {
+                test_index++;
+            }
+        }
+
+        cur_index = Mathf.Max(cur_index,test_index);
+
+        if (cur_index < all_position.Length)
+        {
+            background.DOMove(all_position[cur_index].position,3.0f);
+        }
+    }
+
+}
---
+++ Assets/Script/FishSwim.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FishSwim : MonoBehaviour
+{
+    public float realSpeed;//实际速度
+    public bool die=false;
+    public bool is_the_last = false;
+
+    public Transform show_parent;
+    public Transform target_parent;
+    public Transform[] wander_points;
+    public GameObject[] models;
+    public int index = 0;
+
+    public AudioSource bubble;
+    public AudioSource fall;
+
+    int wander_index = 1;
+
+    Vector3 cur_tar_pos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CheckChangePoint();
+        MyMove();
+        MyRotate();
+    }
+
+    void CheckChangePoint()
+    {
+        if ((transform.position - cur_tar_pos).magnitude <= realSpeed)
+        {
+            float nx = Random.RandomRange(wander_points[0].position.x,wander_points[1].position.x);
+            float ny = Random.RandomRange(wander_points[0].position.y, wander_points[1].position.y);
+            float nz = Random.RandomRange(wander_points[0].position.z, wander_points[1].position.z);
+            cur_tar_pos = new Vector3(nx,ny,nz);
+        }
+    }
+
+    void MyMove()
+    {
+        if (die) return;
+        transform.position += (cur_tar_pos - transform.position).normalized * realSpeed;
+    }
+
+    void MyRotate()
+    {
+        if (die) return;
+        transform.LookAt(cur_tar_pos);
+        transform.eulerAngles = new Vector3(0.0f, transform.eulerAngles.y, 0.0f);
+    }
+
+    public void Die()
+    {
+        if (die) return;
+        die = true;
+        StartCoroutine(Fall());
+    }
+
+    public void MyInit(int id,int endid)
+    {
+        models = new GameObject[show_parent.transform.childCount];
+        for (int i = 0; i < show_parent.transform.childCount; i++)
+        {
+            models[i] = show_parent.transform.GetChild(i).gameObject;
+        }
+
+        if (id != endid)
+        {
+            int rand = Random.Range(0, show_parent.transform.childCount - 1);
+            models[rand].SetActive(true);
+            is_the_last = false;
+        }
+        else
+        {
+            for (int i = 0; i < models.Length; i++)
+            {
+                if (models[i].name == "Shark")
+                {
+                    models[i].SetActive(true);
+                    is_the_last = true;
+                    break;
+                }
+            }
+        }
+
+        wander_points = FindObjectOfType<AllPoints>().all_Points;
+        float nx = Random.Range(wander_points[0].position.x, wander_points[1].position.x);
+        float ny = Random.Range(wander_points[0].position.y, wander_points[1].position.y);
+        float nz = Random.Range(wander_points[0].position.z, wander_points[1].position.z);
+        cur_tar_pos = new Vector3(nx, ny, nz);
+        nx = Random.Range(wander_points[0].position.x, wander_points[1].position.x);
+        ny = Random.Range(wander_points[0].position.y, wander_points[1].position.y);
+        nz = Random.Range(wander_points[0].position.z, wander_points[1].position.z);
+        transform.position = new Vector3(nx, ny, nz);
+    }
+
+    IEnumerator Fall()
+    {
+        float rad = Random.Range(0.0f,2.5f);
+        yield return new WaitForSeconds(rad);
+        transform.DORotate(new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 180), 1.0f);
+        fall.Play();
+        yield return new WaitForSeconds(1.0f);
+        Rigidbody rigid=gameObject.AddComponent<Rigidbody>();
+        yield return 0;
+    }
+
+    public void Revive()
+    {
+        bubble.Play();
+        Destroy(GetComponent<Rigidbody>());
+        die = false;
+        transform.eulerAngles = Vector3.zero;
+    }
+
+
+}
